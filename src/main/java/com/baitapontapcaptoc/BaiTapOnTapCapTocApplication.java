19760f3fa3cdbf4ccd7b7f55f276a407d3644fe1
thehongbaokhanh <thehongbaokhanh2005@gmail.com>
2026-06-02 15:00:16 +0700

    first commit

---
+++ src/main/java/com/baitapontapcaptoc/BaiTapOnTapCapTocApplication.java
@@ -0,0 +1,13 @@
+package com.baitapontapcaptoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaiTapOnTapCapTocApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaiTapOnTapCapTocApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/baitapontapcaptoc/BaiTapOnTapCapTocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.baitapontapcaptoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaiTapOnTapCapTocApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
